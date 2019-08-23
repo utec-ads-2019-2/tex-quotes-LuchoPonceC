dc7583b6cb564ab1150b20dbba8951a7704b30ca
LuchoPonce <luis.ponce@utec.edu.pe>
2019-08-22 21:53:21 -0700

    final_result

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    return 0;
+	
+    string leer;
+	string texto = "";
+	int buscar = 0;
+	while(getline(cin,leer)){
+		for(auto letra : leer){
+			if(letra==34 and buscar == 0){
+				cout<<char(96)<<char(96);
+				buscar++;
+			}
+			else if(letra==34 and buscar == 1){
+				cout<<char(39)<<char(39);
+				buscar--;
+			}
+			else{
+				cout<<letra;
+			}
+		}
+		cout<<endl;
+	}
+	
+	return 0;
 }
